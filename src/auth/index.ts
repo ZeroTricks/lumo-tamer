bfc2fa123bb5b357ee52fd4bfc285b7b204d7660
ZeroTricks <github@undb.me>
2026-01-16 00:36:54 +0100

    add "native" auth method using go-proton-api (untested as can't pass captcha).

---
+++ src/auth/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Auth module exports
+ */
+
+export { AuthManager } from './manager.js';
+export { runProtonAuth } from './go-proton-api/proton-auth-cli.js';
+export { parseRcloneConfig, listRcloneRemotes } from './rclone/index.js';
+export type { AuthConfig, SRPAuthResult, SRPAuthTokens } from './go-proton-api/types.js';
+export type { RcloneProtonConfig } from './rclone/index.js';
